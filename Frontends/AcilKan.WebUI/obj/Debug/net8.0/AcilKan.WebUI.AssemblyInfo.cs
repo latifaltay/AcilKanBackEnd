a271a545c3e53666dac6ddbe6bf535fa30573735
devrimmehmet <devrimmehmet@gmail.com>
2025-03-08 01:21:37 +0300

    deleted frontend.

--- Frontends/AcilKan.WebUI/obj/Debug/net8.0/AcilKan.WebUI.AssemblyInfo.cs
+++ Frontends/AcilKan.WebUI/obj/Debug/net8.0/AcilKan.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcilKan.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb39837e46ea5c009a2107c7f65c207b1c6623c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9275d6bafcf0de70bea04da0b609bd8ce9db23d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcilKan.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcilKan.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
